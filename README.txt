61777930ae1fdff3ff924e5dfdd4bfc3e5266d51
Steven  La <stevenla@s-128-97-122-146.resnet.ucla.edu>
2018-02-05 20:32:53 -0800

    First commit

---
+++ README.txt
@@ -0,0 +1,26 @@
+Spam Assist (2018)
+
+Author: Steven La
+(https://github.com/stevenla14)
+------------------
+About:
+This program was originally written to 'troll' my friend who's been complaining about 
+email spam for a while now. It takes in a file containing a list of emails, and sends 
+the contents of another "spam" message file to each email. This project was completed 
+in the span of 2 days. It is compatible only with Python versions 3 and above.
+
+How To Use:
+1.) Place all emails that you want to 'spam' in a file within the same directory as 'spam.py'. 
+Make sure to place each email on their own line, which means that each pair of emails has a '\n' 
+char in between.
+2.) Place the entire spam message in a file within the same directory as 'spam.py'.
+3.) Run 'spam.py' using Python 3 or above.
+Note: You may need to enable 'Allow less secure apps' on Gmail, to allow a connection from the
+command line. Also, the uploaded files include 'emails.txt' and 'message.txt' which are supposed 
+to be the respective files in Steps 1 and 2, but you can make your own/rename them if you wish. 
+(Just make sure to put in the right file names when prompted.)
+
+Documented Limitations:
+
+1.) Only supports email providers: Gmail, Outlook, Hotmail, Yahoo, AT&T, Comcast, Verizon
+2.) Fairly slow if a lot of emails are within the file...
---
+++ spam.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+import random, sys, smtplib, getpass, os
+
+domain_names = {'gmail': 'smtp.gmail.com', 'outlook': 'smtp-mail.outlook.com', 'hotmail': 'smtp-mail.outlook.com', 'yahoo': 'smtp.mail.yahoo.com', 'at&t': 'smpt.mail.att.net', 'comcast': 'smtp.comcast.net', 'verizon': 'smtp.verizon.net'}
+
+print('Who is your email provider? (Gmail, Yahoo, AT&T, etc.)')
+provider = (input()).lower()
+
+while not provider in domain_names.keys():
+    print('Invalid provider!\nWho is your email provider? (Gmail, Yahoo, AT&T, etc.)')
+    provider = (input()).lower()
+
+print('What is your email ID?')
+email_ID = input()
+
+print ('What is your email extension? (.com, .edu, etc.)')
+extension = input()
+
+password = getpass.getpass(prompt='What is your email password?\n')
+
+full_email = email_ID + '@' + provider + extension
+smtpObj = None
+
+if (provider == 'at&t') or (provider == 'verizon'):
+    smtpObj = smtplib.SMTP_SSL(domain_names[provider], 465)
+    smtpObj.ehlo()
+else:
+    smtpObj = smtplib.SMTP(domain_names[provider], 587)
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    
+smtpObj.login(full_email, password)
+
+print('What is the name of the file containing all of the emails? (Include extension)')
+input_file = input()
+input_fileObj = open(input_file, 'r')
+temp = input_fileObj.readlines()
+
+input_list = []
+for i in temp:
+    if i != '\n':
+        input_list.append(i.strip())
+
+print('What is the name of the file containing the spam message? (Include extension)')
+message_file = input()
+message_fileObj = open(message_file, 'r')
+message_content = message_fileObj.read()
+
+print('Enter the subject for the email:')
+subject = input()
+
+for i in input_list:
+    recipient = i
+    adjusted_subject = 'Subject: ' + subject + '\n'
+    message = message_content.replace('\\n', '\n')
+    smtpObj.sendmail(full_email, recipient, adjusted_subject + message)
+
+smtpObj.quit()
+if not input_list:
+    print('Nothing was sent, since your file containing all of the emails is empty!')
+else:
+    print('Success! Your email(s) have been sent.')
